fbe584502baba89f7fa9e6a2199bb4f935d632c8
Sebaaa14 <sebazxx14@gmail.com>
2023-06-02 21:51:15 -0400

    corregir docentes para que carguen desde el json

--- frontend/src/app/ventanas/docentes/docentes.component.ts
+++ frontend/src/app/ventanas/docentes/docentes.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Profesor {
+  titulo: string;
+  nombre: string;
+  correo: string;
+  horario: string;
+}
 
 @Component({
   selector: 'app-docentes',
   templateUrl: './docentes.component.html',
   styleUrls: ['./docentes.component.scss']
 })
-export class DocentesComponent {
+export class DocentesComponent implements OnInit {
+  profesores: Profesor[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.cargarDatos();
+  }
+
+  cargarDatos() {
+    this.http.get<Profesor[]>('app/json/docentes.json').subscribe(
+      (data) => {
+        this.profesores = data;
+      },
+      (error) => {
+        console.error('Error al cargar los datos de los profesores:', error);
+      }
+    );
+  }
 }
