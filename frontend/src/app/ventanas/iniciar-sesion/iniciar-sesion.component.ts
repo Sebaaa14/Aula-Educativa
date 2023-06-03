6c59e29dbc01e6131d77472ec57abccae52250ca
Sebaaa14 <sebazxx14@gmail.com>
2023-06-02 21:09:56 -0400

    ahora redirige el inicio de sesion

--- frontend/src/app/ventanas/iniciar-sesion/iniciar-sesion.component.ts
+++ frontend/src/app/ventanas/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -10,15 +11,25 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 export class IniciarSesionComponent {
   userForm = this.fb.group({
-    usuario: ['',Validators.compose([
+    usuario: ['', Validators.compose([
       Validators.pattern("^[0-9]{8,9}$"),
       Validators.required
     ])],
-    contrasena: ['',Validators.compose([
+    contrasena: ['', Validators.compose([
       Validators.pattern("^.{6,10}$"),
       Validators.required
     ])],
   });
-  constructor(private fb: FormBuilder){}
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  iniciarSesion() {
+    if (this.userForm.valid) {
+      // Redirige a la otra ventana
+      this.router.navigate(['home']);
+    }
+  }
 }
+
+
 
