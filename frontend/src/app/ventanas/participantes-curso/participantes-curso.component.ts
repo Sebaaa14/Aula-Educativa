8992dfa7913e3815d30824000fe32ebfae74908e
Seba <sebazxx14@gmail.com>
2023-06-11 00:36:39 -0400

    otro json para participantes

--- frontend/src/app/ventanas/participantes-curso/participantes-curso.component.ts
+++ frontend/src/app/ventanas/participantes-curso/participantes-curso.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Alumno {
+  rut: string;
+  nombre: string;
+  apellidoP: string;
+  apellidoM: string;
+  colegio: string;
+}
 
 @Component({
   selector: 'app-participantes-curso',
   templateUrl: './participantes-curso.component.html',
   styleUrls: ['./participantes-curso.component.scss']
 })
-export class ParticipantesCursoComponent {
+export class ParticipantesCursoComponent implements OnInit {
+  alumnos: Alumno[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.cargarAlumnos();
+  }
+
+  cargarAlumnos() {
+    this.http.get<Alumno[]>('assets/json/participantes.json').subscribe(
+      (data: Alumno[]) => {
+        this.alumnos = data;
+      },
+      (error: any) => {
+        console.error('Error al leer el archivo JSON:', error);
+      }
+    );
+  }
 }
