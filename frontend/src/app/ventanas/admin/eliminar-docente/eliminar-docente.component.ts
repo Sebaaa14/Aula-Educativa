2791130ef14c5df6705a1c1d4672b64b157b7167
Seba <sebazxx14@gmail.com>
2023-06-26 16:28:41 -0400

    admin

--- frontend/src/app/ventanas/admin/eliminar-docente/eliminar-docente.component.ts
+++ frontend/src/app/ventanas/admin/eliminar-docente/eliminar-docente.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-eliminar-docente',
   templateUrl: './eliminar-docente.component.html',
   styleUrls: ['./eliminar-docente.component.scss']
 })
-export class EliminarDocenteComponent {
+export class EliminarDocenteComponent implements OnInit {
+  docentes: any[] = [];
+  
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.obtenerDocentes();
+  }
+
+
+  obtenerDocentes() {
+    this.http.get<any>('http://localhost:9000/docentes').subscribe(
+      (response) => {
+        this.docentes = response.data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  eliminarDocente(idDocente: string) {
+    if (idDocente) {
+      this.http.delete(`http://localhost:9000/eliminarDocente/${idDocente}`, { observe: 'response' }).subscribe(
+        (response: any) => {
+          alert("Docente eliminado correctamente");
+        },
+        (error) => {
+          console.error(error);
+          alert("Error al eliminar el Docente");
+        }
+      );
+    } else {
+      alert("Ingrese el nombre del docente");
+    }
+  }
 
 }
