e293dca542e9efb4f12d284a345d3e5bdade0486
Seba <sebazxx14@gmail.com>
2023-06-26 18:46:49 -0400

    commit responsividad

--- frontend/src/app/componentes/footer/footer.component.ts
+++ frontend/src/app/componentes/footer/footer.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from 'src/app/services/authorization/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  constructor(private authService: AuthService, private router: Router) {}
+  sesionIniciada(): boolean {
+    return this.authService.isAuthenticated();
+  }
 }
